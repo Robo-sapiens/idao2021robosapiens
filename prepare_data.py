45e7d8e521a7d36ba5aa43476af7c3e10e0dfce9
sovadim <vadik.sofin@yandex.ru>
2021-03-27 18:46:00 +0300

    fix classes

--- prepare_data.py
+++ prepare_data.py
@@ -4,6 +4,7 @@ import os
 import copy
 import shutil
 from tqdm import tqdm
+import re
 
 ## Paths
 
@@ -13,24 +14,47 @@ val_dir = os.path.join(os.getcwd(), 'tmp', 'val')
 
 ## Classes
 
-classes = ['ER', 'NR']
+classes_folders = ['ER', 'NR']
+
+target_classes = [
+    'ER_1',
+    'ER_3',
+    'ER_6',
+    'ER_10',
+    'ER_20',
+    'ER_30',
+    'NR_1',
+    'NR_3',
+    'NR_6',
+    'NR_10',
+    'NR_20',
+    'NR_30',
+]
 
 ## Make dirs
 
 for dir_name in [train_dir, val_dir]:
-    for class_name in classes:
+    for class_name in target_classes:
         os.makedirs(os.path.join(dir_name, class_name), exist_ok=True)
+
+def parse_class_from_filename(filename):
+    match = re.search(r'(E|N)+R_\d+', filename)
+    return match[0]
+
+def parse_angle_from_filename(filename):
+    match = re.search(r'-*[\d.]+', filename)
+    return match[0]
 
 ## Copy files
 
-for class_name in classes:
-    source_dir = os.path.join(data_root, class_name)
-    for i, file_name in enumerate(tqdm(os.listdir(source_dir))):
+for folder_name in classes_folders:
+    source_dir = os.path.join(data_root, folder_name)
+    for i, filename in enumerate(tqdm(os.listdir(source_dir))):
         if i % 6 != 0:
             dest_dir = os.path.join(train_dir)
         else:
             dest_dir = os.path.join(val_dir)
         shutil.copy(
-            os.path.join(source_dir, file_name),
-            os.path.join(dest_dir, class_name)
+            os.path.join(source_dir, filename),
+            os.path.join(dest_dir, parse_class_from_filename(filename))
         )
