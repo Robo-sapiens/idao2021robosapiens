8dc9958e4283e524f75e8e7a129972477167ced6
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-04 01:13:21 +0000

    Adding numeric_owner as keyword arguement

--- yadisk_loader_dataset.py
+++ yadisk_loader_dataset.py
@@ -47,7 +47,7 @@ for tar_ball in tar_balls:
                 if not is_within_directory(path, member_path):
                     raise Exception("Attempted Path Traversal in Tar File")
         
-            tar.extractall(path, members, numeric_owner) 
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
             
         
         safe_extract(tar, "data")
